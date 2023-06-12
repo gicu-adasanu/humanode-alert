b38f368cb5bf2b0506e827a4600fc1a66ba6855a
Gicu Adasanu <gicu.adasanu@orage.md>
2023-06-12 19:42:53 +0300

    fix import

--- src/main/java/io/humanode/humanodealert/bot/TelegramBot.java
+++ src/main/java/io/humanode/humanodealert/bot/TelegramBot.java
@@ -1,6 +1,5 @@
 package io.humanode.humanodealert.bot;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -11,6 +10,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import javax.annotation.PostConstruct;
 import java.io.*;
 
 @Slf4j
