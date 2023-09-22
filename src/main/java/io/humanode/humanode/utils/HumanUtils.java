bb3580f3240fb20a5e7be4e0ee4d594b90183d0c
gicu <gicu.adasanu098@yandex.com>
2023-09-22 23:46:02 +0300

    update register message

--- src/main/java/io/humanode/humanode/utils/HumanUtils.java
+++ src/main/java/io/humanode/humanode/utils/HumanUtils.java
@@ -1,7 +1,7 @@
 package io.humanode.humanode.utils;
 
 public class HumanUtils {
-    public static final String SUCCESSFULLY_REGISTERED_MESSAGE = "You have successfully registered, you will receive alerts every day (if have enabled notifications) with time left to authenticate and a reminder alert once a minute, 5 minutes before expiry";
+    public static final String SUCCESSFULLY_REGISTERED_MESSAGE = "You have successfully registered, you will receive alerts every day (if have enabled notifications) with time left to authenticate and a reminder alert once a minute, 5 minutes before expiry. To see commands for enable/disable notification type /help";
     public static final String UNKNOWN_COMMAND = "Unknown command, to register for receive alert about your node send command '/register', to set time zone id send command '/timezone time_zone_id'";
     public static final String MISSING_CHAT_ID = "Chat id not found, need to register, to register type '/register' in chat with bot";
     public static final String SUCCESSFULLY_REGISTERED_TIME_ZONE_ID = "You have successfully set time zone id";
